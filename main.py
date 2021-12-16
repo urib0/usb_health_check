47e568812fa7d542729d9e34f12a3c6e221e7255
uribo <ur1b0.dev@gmail.com>
2021-12-17 03:52:18 +0900

    デバイスのセンサ名を出力するようにした

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import serial
 
 BASE_DIR = "/dev/serial/by-id/"
 #BASE_DIR = "/home/pi/test"
+SERIAL_RATE = 9600
 
 deviceList = os.listdir(BASE_DIR)
 
@@ -11,3 +12,10 @@ if len(deviceList) == 0:
     print("デバイスが見つかりませんでした")
     sys.exit()
 
+for device in deviceList:
+    readSer = serial.Serial(BASE_DIR + device, SERIAL_RATE, timeout=3)
+    raw = readSer.readline().decode().replace('\n', '')
+    s = ""
+    for i in range(len(raw.split(";")) - 1):
+        s += raw.split(";")[i].split("=")[0]
+    print(s)
