95c55812f6e104906a03ebc2fece2277b42fc4cb
uribo <ur1b0.dev@gmail.com>
2022-01-26 18:50:31 +0900

    デバイス名の検出をsplitした個数ではなくホワイトリスト式にした

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ BASE_DIR = "/dev/serial/by-id/"
 SERIAL_RATE = 9600
 
 deviceList = os.listdir(BASE_DIR)
+series_list = ["co2","temp","hum","lx","uv","moisture"]
 
 if len(deviceList) == 0:
     print("デバイスが見つかりませんでした")
@@ -16,12 +17,16 @@ for device in deviceList:
     try:
         readSer = serial.Serial(BASE_DIR + device, SERIAL_RATE, timeout=3)
         raw = readSer.readline().decode().replace('\n', '')
-        s = ""
-        for i in range(len(raw.split(";")) - 2):
-            s += raw.split(";")[i].split("=")[0]
-        if len(s) == 0:
-            print("unknown:" + BASE_DIR + device)
+        s = []
+        for i in range(len(raw.split(";"))):
+            s.append(raw.split(";")[i].split("=")[0])
+        device_name = ""
+        for i in range(len(s)):
+            if s[i] in series_list:
+                device_name += s[i]
+        if len(device_name) == 0:
+           print("unknown:" + BASE_DIR + device)
         else:
-            print(s + ":" + BASE_DIR + device)
-    except requests.exceptions.RequestException as e:
-        print('request failed: ', e)
+            print(device_name + ":" + BASE_DIR + device)
+    except Exception as e:
+        print('failed: ', e)
